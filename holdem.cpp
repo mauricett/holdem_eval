a188405fc9a2d392a223dcae17ae808a907c209d
mauricett <beg0rpoker@yahoo.co.uk>
2016-11-02 20:57:26 +0100

    Update holdem.cpp

--- holdem.cpp
+++ holdem.cpp
@@ -47,8 +47,8 @@ int merged[cardcnt * player]; //ohne suits
 //MASTER PLAN: 
 //"master funktion" in der alle allocations von eval_ranks gemacht werden
 //und dann eval_ranks in der funktion callen
-//so sparn wir uns die ständigen allocations.
-//evtl einen zusammenhängenden speicher allocaten?!
+//so sparn wir uns die stÃ¤ndigen allocations.
+//evtl einen zusammenhÃ¤ngenden speicher allocaten?!
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
@@ -58,21 +58,18 @@ int merged[cardcnt * player]; //ohne suits
 
 int main()
 {
-	int board[10]{ 0,5, 4, 1, 2, 3, 3, 3 , 0, 3 }; // = { (rank1, rank2 ...), (suit1, suit2 ...) }; // glaube ist besser, weil wir ja sortieren müssen
-	int hands[6 * 2 * 2]{ 6, 6, 0, 1 }; // (rank1, rank2, ...), (suit1, suit2 ...), ...
+	//int board[10]{ 0,5, 4, 1, 2, 3, 3, 3 , 0, 3 }; // = { (rank1, rank2 ...), (suit1, suit2 ...) };n
+	//int hands[6 * 2 * 2]{ 6, 6, 0, 1 }; // (rank1, rank2, ...), (suit1, suit2 ...), ...
 
-	/*0 = win or tie; 1 = total number of players; 2 = number of winning/tieing players; 3 = winning rank; 4 to 9 = players; 
-	z.b. win -> 2 == 1, 4 == winner; 5 to up to 9 == losers
-	oder tie -> 2 == number of tying;*/
-	int result[1 + 2 * 6] = {}; //result[0] = anzahl der simulierten hände; result[1 + 2*p] = player p's wins; result[1 + 2*p + 1] = player p's ties
+	int result[1 + 2 * 6] = {}; //result[0] = anzahl der simulierten hÃ¤nde; result[1 + 2*p] = player p's wins; result[1 + 2*p + 1] = player p's ties
 	double tie[7] = {}; //count tie equity ... tie/playrs_left ... tie[6] == #ties
 
 
 	/*int board1[10] = { 1, 3, 1, 4, 7, 0, 0, 0, 0, 0 };
 	int hands1[24] = { 2, 5, 0, 0, 4, 5, 0, 0, 10, 12, 0, 0, 5, 6, 0, 0 };*/
 	//int board1[10] = { 1, 2, 3, 4, 10, 1, 1, 0, 0, 3 };
-	//int hands1[24] = { 6, 12, 2, 0, 3, 4, 0, 0, 0, 12, 1, 1, 6, 12, 1, 3 }; //getting bad results here
-	//int hands1[24] = { 2, 3, 0, 1, 7, 8, 2, 3, 10, 11, 1, 1, 5, 5, 0, 1 }; //a little bit better results .. looks like problem with flushs, straights or wheelie
+	//int hands1[24] = { 6, 12, 2, 0, 3, 4, 0, 0, 0, 12, 1, 1, 6, 12, 1, 3 }; 
+	//int hands1[24] = { 2, 3, 0, 1, 7, 8, 2, 3, 10, 11, 1, 1, 5, 5, 0, 1 }; 
 	int hands1[24] = { 0, 1, 0, 0, 11, 12,0, 0 };
 	int board1[10] = { 12, 5, 12, 12, 10, 3, 0, 2, 1, 2 };
 	//int board1[10] = { 1, 1, 9, 9, 9, 1, 2, 0, 0, 2 };
@@ -80,8 +77,6 @@ int main()
 	/*generate_board(board1, hands1, cds, 4);
 	find_winner(board1, hands1, merged, result, tie, 4);*/
 	
-
-	//problemo Board: Ac 7s Ah Ad Qh
 
 	int draw = 0;
 	/*
